a4f07857f6386c023c3cc3b4a2a94bc4e0fc4a20
Doppamin <74320273+Doppamin@users.noreply.github.com>
2021-03-10 12:43:37 +0300

    Update suratYap.java

--- MySpecialProjects/src/failedAttemp/suratYap.java
+++ MySpecialProjects/src/failedAttemp/suratYap.java
@@ -3,7 +3,8 @@ package failedAttemp;
 public class suratYap {
 
     /**
-     * @param args the command line arguments
+     this is cancelled project . The point was you could create your face choosing eyes and mouth include in strings.
+     you could still change string a and b manually with punctiation and see great funny faces 
      */
     public static void main(String[] args) {
         // TODO code application logic here
